b7bf5eba9515b1e3b074460ed99be6c5f2bc218c
tabbash-1 <tabbash.abd22@gmail.com>
2026-08-25 14:19:04 +0300

    Test combo aware swing quality tiers

--- tests/test_horizon_fit_policy.py
+++ tests/test_horizon_fit_policy.py
@@ -1,20 +1,22 @@
 import horizon_fit_policy as h
 
 
-def test_near_threshold_routes_to_swing_not_quick_without_breakout():
-    x=h.classify(direction='LONG',score=65,threshold=68,votes=4,relative_volume=1.1,tactical_rr=1.2,breakout_confirmed=False,production_qualified=False,execution_ready=False,obstacle_reason='VERY_CLOSE_PRIOR_STRUCTURE')
+def test_near_threshold_routes_to_neutral_swing_without_breakout():
+    x=h.classify(symbol='XRPUSDT',direction='LONG',score=65,threshold=68,votes=4,relative_volume=1.1,tactical_rr=1.2,breakout_confirmed=False,production_qualified=False,execution_ready=False,obstacle_reason='VERY_CLOSE_PRIOR_STRUCTURE')
     assert x['quick']['status']=='WATCH_ONLY'
     assert x['swing']['status']=='SWING_RESEARCH_WATCH'
+    assert x['swing']['swing_quality_tier']=='NEUTRAL'
     assert x['preferred_horizon']=='SWING_12_24H'
     assert x['production_threshold_changed'] is False
     assert x['production_score_adjustment'] == 0
 
 
-def test_calibrated_very_close_becomes_research_priority_only():
-    x=h.classify(direction='LONG',score=65,threshold=68,votes=4,relative_volume=.5,tactical_rr=.8,breakout_confirmed=False,production_qualified=False,execution_ready=False,obstacle_reason='VERY_CLOSE')
+def test_high_quality_combo_becomes_research_priority_only():
+    x=h.classify(symbol='BTCUSDT',direction='LONG',score=65,threshold=68,votes=4,relative_volume=.5,tactical_rr=.8,breakout_confirmed=False,production_qualified=False,execution_ready=False,obstacle_reason='VERY_CLOSE')
     assert x['quick']['status']=='WATCH_ONLY'
-    assert x['swing']['status']=='SWING_RESEARCH_PRIORITY'
-    assert x['swing']['research_priority'] is True
+    assert x['swing']['status']=='SWING_RESEARCH_PRIORITY_HIGH'
+    assert x['swing']['swing_quality_tier']=='HIGH'
+    assert x['swing']['swing_quality_evidence']['sample_12h'] == 17
     assert x['swing']['production_qualified'] is False
     assert x['swing']['can_override_production'] is False
     assert x['swing']['production_score_adjustment'] == 0
@@ -22,31 +24,42 @@ def test_calibrated_very_close_becomes_research_priority_only():
     assert x['production_score_adjustment'] == 0
 
 
-def test_priority_does_not_apply_below_research_band():
-    x=h.classify(direction='LONG',score=59,threshold=68,votes=4,relative_volume=2,tactical_rr=3,breakout_confirmed=True,production_qualified=False,execution_ready=False,obstacle_reason='VERY_CLOSE')
+def test_negative_combo_is_deprioritized_not_promoted():
+    x=h.classify(symbol='HYPEUSDT',direction='SHORT',score=65,threshold=68,votes=4,relative_volume=.5,tactical_rr=.8,breakout_confirmed=False,production_qualified=False,execution_ready=False,obstacle_reason='VERY_CLOSE')
+    assert x['swing']['status']=='SWING_RESEARCH_DEPRIORITIZED'
+    assert x['swing']['swing_quality_tier']=='LOW'
+    assert x['swing']['swing_quality_evidence']['mean_12h_pct'] < 0
+    assert x['swing']['production_score_adjustment'] == 0
+    assert x['production_override_allowed'] is False
+
+
+def test_combo_priority_does_not_apply_below_research_band():
+    x=h.classify(symbol='BTCUSDT',direction='LONG',score=59,threshold=68,votes=4,relative_volume=2,tactical_rr=3,breakout_confirmed=True,production_qualified=False,execution_ready=False,obstacle_reason='VERY_CLOSE')
     assert x['swing']['status']=='SWING_WATCH'
-    assert x['swing']['research_priority'] is False
+    assert x['swing']['swing_quality_tier']=='UNRATED'
 
 
 def test_quick_requires_strict_breakout_confirmation():
-    x=h.classify(direction='SHORT',score=64,threshold=68,votes=4,relative_volume=.9,tactical_rr=1.1,breakout_confirmed=True,production_qualified=False,execution_ready=False,obstacle_reason='VERY_CLOSE')
+    x=h.classify(symbol='XRPUSDT',direction='SHORT',score=64,threshold=68,votes=4,relative_volume=.9,tactical_rr=1.1,breakout_confirmed=True,production_qualified=False,execution_ready=False,obstacle_reason='VERY_CLOSE')
     assert x['quick']['status']=='QUICK_TRADE_SHADOW'
     assert x['quick']['evaluation_horizons']==['1h','3h']
     assert x['quick']['can_override_production'] is False
 
 
 def test_production_ready_remains_production_ready():
-    x=h.classify(direction='LONG',score=72,threshold=68,votes=4,relative_volume=1.2,tactical_rr=1.4,breakout_confirmed=True,production_qualified=True,execution_ready=True,obstacle_reason='VERY_CLOSE')
+    x=h.classify(symbol='BTCUSDT',direction='LONG',score=72,threshold=68,votes=4,relative_volume=1.2,tactical_rr=1.4,breakout_confirmed=True,production_qualified=True,execution_ready=True,obstacle_reason='VERY_CLOSE')
     assert x['swing']['status']=='SWING_PRODUCTION_READY'
+    assert x['swing']['swing_quality_tier']=='PRODUCTION'
     assert x['preferred_horizon']=='SWING_12_24H'
     assert x['production_override_allowed'] is False
     assert x['production_score_adjustment'] == 0
 
 
 if __name__=='__main__':
-    test_near_threshold_routes_to_swing_not_quick_without_breakout()
-    test_calibrated_very_close_becomes_research_priority_only()
-    test_priority_does_not_apply_below_research_band()
+    test_near_threshold_routes_to_neutral_swing_without_breakout()
+    test_high_quality_combo_becomes_research_priority_only()
+    test_negative_combo_is_deprioritized_not_promoted()
+    test_combo_priority_does_not_apply_below_research_band()
     test_quick_requires_strict_breakout_confirmation()
     test_production_ready_remains_production_ready()
     print('horizon fit policy tests: ok')
